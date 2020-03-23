200b7c1922d7fbf384c7ee1436f1114f15fc1c81
Vivek Pillai <vpillai@ctisinc.com>
2020-03-23 19:53:23 -0400

    docker...

--- dockerstartapp.bat
+++ dockerstartapp.bat
@@ -1,2 +1,3 @@
-rem docker run --expose 4811 --env PORT=4811 -p 4811:4811 --add-host database:192.168.1.183 --name govex_api aarryann/doserver
-docker run -d --expose 4811 --env PORT=4811 -p 4811:4811 --add-host database:192.168.208.158 --name govex_api aarryann/doserver
+docker run -d --expose 4811 --env PORT=4811 -p 4811:4811 --add-host database:192.168.1.183 --name govex_api_wifi aarryann/doserver
+rem docker run -d --expose 4811 --env PORT=4811 -p 4811:4811 --add-host database:192.168.208.158 --name govex_api_lan aarryann/doserver
+rem docker run -d --expose 4811 --env PORT=4811 -p 4811:4811 --add-host database:172.17.210.49 --name govex_api aarryann/doserver
