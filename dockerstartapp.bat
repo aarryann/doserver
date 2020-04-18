5b5a46111aaec6863fcd297dd651f0c8c0f54654
Vic <aarryann@yahoo.com>
2020-04-17 20:31:32 -0400

    apollo...

--- dockerstartapp.bat
+++ dockerstartapp.bat
@@ -1,3 +1,3 @@
-docker run -d --expose 4811 --env PORT=4811 -p 4811:4811 --add-host database:192.168.1.183 --name govex_api_wifi aarryann/doserver
+docker run -d --expose 4811 --env PORT=4811 -p 4811:4811 --add-host database:192.168.1.202 --name govex_api_wifi aarryann/doserver
 rem docker run -d --expose 4811 --env PORT=4811 -p 4811:4811 --add-host database:192.168.208.158 --name govex_api_lan aarryann/doserver
 rem docker run -d --expose 4811 --env PORT=4811 -p 4811:4811 --add-host database:172.17.210.49 --name govex_api aarryann/doserver
